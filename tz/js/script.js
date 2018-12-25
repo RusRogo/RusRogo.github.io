4ec5f7f2fc90e891e00c89142977e2c65a825546
Rus <rusf6996@mail.ru>
2018-12-25 16:41:30 +0300

    25,12-17

--- tz/js/script.js
+++ tz/js/script.js
@@ -1,9 +1,4 @@
 
-function show(state, id){
-  if(id!=null)
-    document.getElementById(id).style.display = state;
-  else{
-    document.getElementById('wrap__popup').style.display = state;
-  }
-  document.getElementById('fogging').style.display = state;
+function show(state, id) {
+  document.getElementById('wrap__popup').style.display = state;
 }

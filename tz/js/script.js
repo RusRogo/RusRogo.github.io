92615b31981c99b7da699665bed5686ab2b471ad
Rus <rusf6996@mail.ru>
2018-12-26 17:07:39 +0300

    26,12-17

--- tz/js/script.js
+++ tz/js/script.js
@@ -1,5 +1,19 @@
+// 1-ая JS функция на показ и скрытие popup окна
 
-// function show(state, id) {
-//   document.getElementById('wrap__popup').style.display = state;
-// }
+// Открытие popup
+const button = document.getElementById('button');
+button.addEventListener('click', function() {
+   document.getElementById('wrap__popup').style.display = 'block';
+});
 
+// закрытие popup окна при клике вне его области
+const overlay = document.getElementById('overlay');
+overlay.addEventListener('click', function() {
+   document.getElementById('wrap__popup').style.display = 'none';
+});
+
+// Закрытие popup при клике на "крестик"
+const close_popup = document.getElementById('close_popup');
+close_popup.addEventListener('click', function() {
+   document.getElementById('wrap__popup').style.display = 'none';
+});

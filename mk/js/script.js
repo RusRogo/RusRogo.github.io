4d708c93eaff129dd60572bf4744859cdd2ad0b7
Rus <rusf6996@mail.ru>
2019-01-03 00:56:17 +0300

    3,01-1

---
+++ mk/js/script.js
@@ -0,0 +1,32 @@
+
+// slick
+$('.directions__slider').slick({
+  slidesToShow: 3,
+  // autoplay: true, 
+  autoplaySpeed: 2500,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+  
+// Стрелки слайдер
+var slick_prev = document.querySelector('.slick-prev');
+var directions__slider_prev = document.querySelector('.directions__slider_prev');
+var slick_next = document.querySelector('.slick-next');
+var directions__slider_next = document.querySelector('.directions__slider_next');
+var event = new Event("click");
+
+directions__slider_prev.addEventListener('click', function() {
+  slick_prev.dispatchEvent(event);
+});
+directions__slider_next.addEventListener('click', function() {
+  slick_next.dispatchEvent(event);
+});
+
+console.log('Fine!');
